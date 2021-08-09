e7faa7df2e64c7e7881d44f3e54cd3e4e42cc661
Joao Victor Rodrigues dos Santos <jvrdsantos@hotmail.com>
2021-08-09 19:52:42 -0300

    Tarefa Docker Joao Victor Rodrigues dos Santos

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,12 +1,29 @@
+#Aluno : João Victor Rodrigues dos Santos
+
+#Obs acessar pelo local host:80/docs
 import pickle
 from fastapi import FastAPI
-
 app = FastAPI()
+
 @app.post('/model')
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
-    with open('model/Titanic.pkl', 'rb') as fid: 
+
+async def titanic(Sex:int,Age:float, Lifeboat:int, Pclass:int):
+    with open('model/Titanic.pkl', 'rb') as fid:
         titanic = pickle.load(fid)
+
+    predicao = titanic.predict([[Sex, Age, Lifeboat, Pclass]]).tolist()
+
+    try:
+        return {
+            'status': 200,
+            'survived': bool(predicao[0]),
+            'message': 'Modelo Titanic Executado',
+        }
+    except Exception:
+        return {
+            'message': 'Modelo Titanic não Executado'
+        }
 
 @app.get('/model')
 def get():
